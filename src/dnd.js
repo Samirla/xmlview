05b27f90389c7955f025230d785792a1f209b07d
Sergey Chikuyonok <serge.che@gmail.com>
2011-02-09 20:33:55 +0300

    Extract DND feedback as separate module

--- src/dnd.js
+++ src/dnd.js
@@ -6,22 +6,14 @@
  * 
  * @include "controller.js"
  * @include "dom.js"
+ * @include "dnd_feedback.js"
  */
 (function(){
-	var canvas = document.createElement('canvas'),
-		/** @type {CanvasRenderingContext2D} */
-		ctx = canvas.getContext('2d'),
-		bg = new Image,
-		font_size = 11,
-		font = 'normal ' + font_size + 'px "Lucida Grande", sans-serif',
-		padding_left = 6,
-		padding_top = 4,
-		bg_pattern,
-		
-		/** @type {Element} */
-		drag_elem,
+	/** @type {Element} */
+	var drag_elem,
 		is_dragging = false,
 		data_transfer,
+		dnd_image = new Image,
 		
 		/** @type {Element} */
 		source_node,
@@ -31,12 +23,6 @@
 		ALT_KEY = 2,
 		SHIFT_KEY = 4,
 		CTRL_KEY = 8;
-	
-	bg.onload = function() {
-		bg_pattern = ctx.createPattern(bg, 'repeat');
-	};
-	
-	bg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAbCAIAAAA70dJZAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHdJREFUeNp0j1EOwCAIQ8Fw/3t6CGW4sg7N1kQ/6qNU7b3POd1dRFpIHqlquOZFYb3vQFqcDz4i9dbiMQnKOAwwebqGMv99mJQIy+M2LBtjgFrzW6TsMpRlRMMmrmg1/ORX3tHHKp957J//r18++2/9cj8V1iXAAFG7oYASmw8VAAAAAElFTkSuQmCC';
 	
 	/**
 	 * Creates modifier keys mask from passed event
@@ -67,59 +53,6 @@
 		return c == 16 || c == 17 || c == 18 || c == 91;
 	}
 	
-	function roundedRect(ctx, x, y, width, height, radius) {
-		ctx.beginPath();
-		ctx.moveTo(x + radius, y);
-		ctx.lineTo(x + width - radius, y);
-		ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-		ctx.lineTo(x + width, y + height - radius);
-		ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-		ctx.lineTo(x + radius, y + height);
-		ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-		ctx.lineTo(x, y + radius);
-		ctx.quadraticCurveTo(x, y, x + radius, y);
-		ctx.closePath();
-	}
-		
-	function drawFeedback(text) {
-		ctx.font = font;
-		
-		var tx = ctx.measureText(text),
-			width = padding_left * 2 + tx.width,
-			height = font_size + padding_top * 2;
-		
-		canvas.width = width + 2;
-		canvas.height = height + 2;
-		
-		ctx.save();
-		
-		ctx.globalAlpha = 0.8;
-		
-		ctx.fillStyle = bg_pattern;
-		roundedRect(ctx, 0, 0, width, height, 5);
-		ctx.fill();
-		
-		// inner stroke
-		roundedRect(ctx, 1, 1, width - 1, height - 1, 5);
-		ctx.strokeStyle = '#f1f1f1';
-		ctx.lineWidth = 2;
-		ctx.stroke();
-		
-		// outer stroke
-		roundedRect(ctx, 0.5, 0.5, width, height, 5);
-		ctx.strokeStyle = '#bdbdbd';
-		ctx.lineWidth = 1;
-		ctx.stroke();
-		
-		
-		ctx.fillStyle = '#000';
-		ctx.textBaseline = 'middle';
-		ctx.font = font;
-		ctx.fillText(text, padding_left, Math.round(height / 2));
-		
-		ctx.restore();
-	}
-	
 	/**
 	 * Return attribute quote for XPath
 	 * TODO get from settings
@@ -141,6 +74,11 @@
 			"'": '&apos;'
 		};
 		return text.replace(new RegExp(quote, 'g'), quote_map[quote]);
+	}
+	
+	function updateTransferImage() {
+		if (data_transfer)
+			data_transfer.setDragImage(dnd_image, 6, dnd_image.height);
 	}
 	
 	/**
@@ -212,11 +150,12 @@
 			state = getTransferForAttrName(evt);
 			
 		if (state !== null) {
-			drawFeedback(state);
+			dnd_image.src = xv_dnd_feedback.draw(state);
+//			drawFeedback(state);
 			
 			if (is_dragging) {
 				data_transfer.setData('text/plain', state);
-				data_transfer.setDragImage(canvas, padding_left, canvas.height);
+				updateTransferImage();
 			} else {
 				xv_dom.removeClass(dnd_tooltip, 'xv-dnd-tooltip-hidden');
 				xv_dom.setText(dnd_tooltip, state);
@@ -238,6 +177,8 @@
 	function isHoverElement(elem) {
 		return xv_dom.hasClass(elem, 'xv-tag-name') || xv_dom.hasClass(elem, 'xv-attr-name');
 	}
+	
+	dnd_image.onload = updateTransferImage;
 	
 	// init module
 	xv_signals.documentProcessed.addOnce(function() {
@@ -296,9 +237,5 @@
 				updateTransferState(evt);
 			}
 		});
-			
-		canvas.className = 'xv-drag-image';
-		canvas.width = canvas.height = 1;
-		(document.body || document.documentElement).appendChild(canvas);
 	});
 })();
---
+++ src/dnd_feedback.js
@@ -0,0 +1,89 @@
+/**
+ * Feedback module for drag’n’drop operations: returns a data:url-encoded image
+ * of text that will be dragged
+ * 
+ * @author Sergey Chikuyonok (serge.che@gmail.com)
+ * @link http://chikuyonok.ru
+ */
+var xv_dnd_feedback = (function(){
+	var canvas = document.createElement('canvas'),
+		/** @type {CanvasRenderingContext2D} */
+		ctx = canvas.getContext('2d'),
+		font_size = 11,
+		font = 'normal ' + font_size + 'px "Lucida Grande", sans-serif',
+		padding_left = 6,
+		padding_top = 4,
+		bg_pattern;
+	
+	var bg = new Image;
+	bg.onload = function() {
+		bg_pattern = ctx.createPattern(bg, 'repeat');
+	};
+	bg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAbCAIAAAA70dJZAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHdJREFUeNp0j1EOwCAIQ8Fw/3t6CGW4sg7N1kQ/6qNU7b3POd1dRFpIHqlquOZFYb3vQFqcDz4i9dbiMQnKOAwwebqGMv99mJQIy+M2LBtjgFrzW6TsMpRlRMMmrmg1/ORX3tHHKp957J//r18++2/9cj8V1iXAAFG7oYASmw8VAAAAAElFTkSuQmCC';
+	
+	function roundedRect(ctx, x, y, width, height, radius) {
+		ctx.beginPath();
+		ctx.moveTo(x + radius, y);
+		ctx.lineTo(x + width - radius, y);
+		ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+		ctx.lineTo(x + width, y + height - radius);
+		ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+		ctx.lineTo(x + radius, y + height);
+		ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+		ctx.lineTo(x, y + radius);
+		ctx.quadraticCurveTo(x, y, x + radius, y);
+		ctx.closePath();
+	}
+	
+	canvas.className = 'xv-drag-image';
+	canvas.width = canvas.height = 1;
+	(document.body || document.documentElement).appendChild(canvas);
+	
+	return {
+		/**
+		 * Draw feedback image with <code>text</code> in it
+		 * @param {String} text
+		 * @return {String} data:url of image
+		 */
+		draw: function(text) {
+			ctx.font = font;
+		
+			var tx = ctx.measureText(text),
+				width = padding_left * 2 + tx.width,
+				height = font_size + padding_top * 2;
+			
+			canvas.width = width + 2;
+			canvas.height = height + 2;
+			
+			ctx.save();
+			
+			ctx.globalAlpha = 0.8;
+			
+			ctx.fillStyle = bg_pattern;
+			roundedRect(ctx, 0, 0, width, height, 5);
+			ctx.fill();
+			
+			// inner stroke
+			roundedRect(ctx, 1, 1, width - 1, height - 1, 5);
+			ctx.strokeStyle = '#f1f1f1';
+			ctx.lineWidth = 2;
+			ctx.stroke();
+			
+			// outer stroke
+			roundedRect(ctx, 0.5, 0.5, width, height, 5);
+			ctx.strokeStyle = '#bdbdbd';
+			ctx.lineWidth = 1;
+			ctx.stroke();
+			
+			
+			ctx.fillStyle = '#000';
+			ctx.textBaseline = 'middle';
+			ctx.font = font;
+			ctx.fillText(text, padding_left, Math.round(height / 2));
+			
+			ctx.restore();
+			
+			return canvas.toDataURL();
+		}
+	};
+})();
